835d3891c5d46c1248e2b132c15679adfc23ac8a
Nasa Quba <nasaquba@gmail.com>
2018-10-16 23:41:09 -0700

    fizxed bug

--- client/src/components/Chart1.jsx
+++ client/src/components/Chart1.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 const chart = require('chart.js');
+import $ from 'jquery';
+
 
 class Chart1 extends React.Component {
-  super(props)
-  this.state {
-    data: null
-  }
-}
-
-componentDidMount() {
-  $.ajax({
-    url: '/getAllCases',
-    method: 'GET',
-    success: (data) => {
-      debugger;
-    },
-
-    error: (err) => {
-      debugger
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
     }
-    });
   }
 
+  componentDidMount() {
+    $.ajax({
+      url: '/getAllCases',
+      method: 'GET',
+      success: (data) => {
+        debugger;
+      },
 
-render () {
-  return (<div> timeline render</div>)
+      error: (err) => {
+        debugger
+      }
+      });
+    }
+
+
+  render () {
+    return (<div> timeline render </div>)
+  }
+
 }
-
-
 
 export default Chart1;
