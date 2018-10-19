9a56e3ac8b32fb26048beb8bec47b9b4ae3667d1
Nasa Quba <nasaquba@gmail.com>
2018-10-18 20:57:45 -0700

    added timeline component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Chart1 from './Chart1.jsx'
+
+import CaseTimeline from './CaseTimeline.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -11,15 +11,10 @@ class App extends React.Component {
     }
   }
 
-  // componentDidMount() {
-  //   $.ajax({
-  //     });
-  // }
 
   render () {
-
     return (<div>
-      <Chart1 />
+      <CaseTimeline />
     </div>)
   }
 }
